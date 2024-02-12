3aced91f8dc4c96e2698adb4142f970018dba09c
Gary Dong <garydong@Garys-MacBook-Pro.local>
2024-02-12 13:18:21 -0500

    added in logs and views

--- app/src/main/java/com/example/activitieslifecycle/MainActivity.kt
+++ app/src/main/java/com/example/activitieslifecycle/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.activitieslifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.example.activitieslifecycle.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+
+private var TAG = "MainActivity"
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d(TAG, "onCreate() called")
+
+        binding.helloButton.setOnClickListener{view ->
+            binding.edittext.setText("hello")
+            binding.textview.setText("hello")
+        }
+
+
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+
 }
